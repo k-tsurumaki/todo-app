259794300fed4bb69a8697d70cc7800f48e03d77
k-tsurumaki <kazuki67266726@gmail.com>
2025-02-23 15:05:40 +0900

    add create todo func

--- main.go
+++ main.go
@@ -16,24 +16,27 @@ func main() {
 
 	fmt.Println(models.DB)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "text@example.com"
-	// u.Password = "testtest"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "text@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
 
-	// u.CreateUser()
-
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 	
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	user, _ := models.GetUser(1)
+	user.CreateTodo("Hello World")
 }
