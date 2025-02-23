afbd8e23ecfc4e82cca5c7a182b756d243adee81
k-tsurumaki <kazuki67266726@gmail.com>
2025-02-23 14:36:34 +0900

    add get user func

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ func main() {
 
 	fmt.Println(models.DB)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "text@example.com"
-	u.Password = "testtest"
-	fmt.Println(u)
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "text@example.com"
+	// u.Password = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
